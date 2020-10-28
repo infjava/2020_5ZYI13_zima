ee850e0ba81b5cffab9167200e95a5d3682bfdce
Jan Janech <jan.janech@kst.uniza.sk>
2020-10-28 13:35:02 +0100

    05b - odrazanie od horneho a dolneho okraju

--- pong/Lopta.java
+++ pong/Lopta.java
@@ -3,6 +3,8 @@ import java.util.Random;
 public class Lopta {
     private Kruh grafika;
     
+    private int poziciaY;
+    
     private int smerX;
     private int smerY;
     
@@ -13,10 +15,10 @@ public class Lopta {
         
         this.grafika.zmenPriemer(10);
         
-        int poziciaY = nahodneCisla.nextInt(281) + 10;
+        this.poziciaY = nahodneCisla.nextInt(281) + 10;
         
         this.grafika.posunVodorovne(125);
-        this.grafika.posunZvisle(poziciaY - 65);
+        this.grafika.posunZvisle(this.poziciaY - 65);
         
         if (nahodneCisla.nextBoolean()) {
             this.smerX = +10;
@@ -34,10 +36,16 @@ public class Lopta {
     }
     
     public void posun() {
+        if (this.poziciaY < 6 || this.poziciaY > 294) {
+            this.smerY = -this.smerY;
+        }
+        
         this.grafika.skry();
         
         this.grafika.posunVodorovne(this.smerX);
         this.grafika.posunZvisle(this.smerY);
+        
+        this.poziciaY = this.poziciaY + this.smerY;
         
         this.grafika.zobraz();
     }
