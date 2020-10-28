4a2e65a27598a3fd561dc07f33ae20086d611001
Jan Janech <jan.janech@kst.uniza.sk>
2020-10-28 13:03:12 +0100

    05b - nahodna pozicia lopticky

--- pong/Lopta.java
+++ pong/Lopta.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Lopta {
     private Kruh grafika;
     
@@ -5,15 +7,28 @@ public class Lopta {
     private int smerY;
     
     public Lopta() {
+        Random nahodneCisla = new Random();
+        
         this.grafika = new Kruh();
         
         this.grafika.zmenPriemer(10);
         
-        this.grafika.posunVodorovne(125);
-        this.grafika.posunZvisle(85);
+        int poziciaY = nahodneCisla.nextInt(281) + 10;
         
-        this.smerX = +10;
-        this.smerY = +10;
+        this.grafika.posunVodorovne(125);
+        this.grafika.posunZvisle(poziciaY - 65);
+        
+        if (nahodneCisla.nextBoolean()) {
+            this.smerX = +10;
+        } else {
+            this.smerX = -10;
+        }
+        
+        if (nahodneCisla.nextBoolean()) {
+            this.smerY = +10;
+        } else {
+            this.smerY = -10;
+        }
         
         this.grafika.zobraz();
     }
