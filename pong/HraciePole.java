db0027302bcaccb85377f5f0465ea21637cd0975
Jan Janech <jan.janech@kst.uniza.sk>
2020-11-03 13:53:04 +0100

    06a - kompozit pre hru pong

---
+++ pong/HraciePole.java
@@ -0,0 +1,37 @@
+public class HraciePole {
+    private Lopta lopta;
+    private Pad lavyPad;
+    private Pad pravyPad;
+    private Manazer manazer;
+    
+    public HraciePole() {
+        this.lopta = new Lopta();
+        this.lavyPad = new Pad(false);
+        this.pravyPad = new Pad(true);
+        this.manazer = new Manazer();
+    }
+    
+    public void posunDole() {
+        this.pravyPad.posunDole();
+    }
+    
+    public void posunHore() {
+        this.pravyPad.posunHore();
+    }
+    
+    public void posunVlavo() {
+        this.lavyPad.posunHore();
+    }
+    
+    public void posunVpravo() {
+        this.lavyPad.posunDole();
+    }
+    
+    public void tik() {
+        this.lopta.posun();
+    }
+    
+    public void spusti() {
+        this.manazer.spravujObjekt(this);
+    }
+}
