ee7ef2d6aa1bdd58c9f4e3979ece76e458ea2d84
Jan Janech <jan.janech@kst.uniza.sk>
2020-11-03 13:52:49 +0100

    06a - pod sa da posuvat

--- pong/Pad.java
+++ pong/Pad.java
@@ -1,12 +1,16 @@
 public class Pad {
     private Obdlznik grafika;
     
+    private int poziciaY;
+    
     public Pad(boolean pravyPod) {
         this.grafika = new Obdlznik();
         
         this.grafika.zmenStrany(5, 50);
         
         this.grafika.posunZvisle(75);
+        
+        this.poziciaY = 150;
         
         if (pravyPod) {
             this.grafika.posunVodorovne(235);
@@ -15,5 +19,19 @@ public class Pad {
         }
         
         this.grafika.zobraz();
+    }
+    
+    public void posunDole() {
+        if (this.poziciaY <= 270) {
+            this.grafika.posunZvisle(5);
+            this.poziciaY += 5;
+        }
+    }
+    
+    public void posunHore() {
+        if (this.poziciaY >= 30) {
+            this.grafika.posunZvisle(-5);
+            this.poziciaY -= 5;
+        }
     }
 }
