5f25ec4c6f118f5c32cf08c40a21c566c1446372
Jan Janech <jan.janech@kst.uniza.sk>
2020-11-03 13:17:52 +0100

    06a - lavy aj pravy Pod

--- pong/Pad.java
+++ pong/Pad.java
@@ -1,13 +1,18 @@
 public class Pad {
     private Obdlznik grafika;
     
-    public Pad() {
+    public Pad(boolean pravyPod) {
         this.grafika = new Obdlznik();
         
         this.grafika.zmenStrany(5, 50);
         
         this.grafika.posunZvisle(75);
-        this.grafika.posunVodorovne(-60);
+        
+        if (pravyPod) {
+            this.grafika.posunVodorovne(235);
+        } else {
+            this.grafika.posunVodorovne(-60);
+        }
         
         this.grafika.zobraz();
     }
