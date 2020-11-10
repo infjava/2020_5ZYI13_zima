7cb7aa0a13ca82b844ce203540bcd7d9b9bdfb71
Jan Janech <jan.janech@kst.uniza.sk>
2020-11-10 14:18:41 +0100

    07a - vklad na ucet/vyber z uctu a ziskavanie stavu uctu

--- banka/Ucet.java
+++ banka/Ucet.java
@@ -12,4 +12,26 @@ public class Ucet {
     public String getIban() {
         return this.iban;
     }
+    
+    public void vyber(int eur, int centov) {
+        if (centov < 100) {
+            if (eur >= 0 && centov >= 0) {
+                if (eur * 100 + centov <= this.sumaVCentoch) {
+                    this.sumaVCentoch -= eur * 100 + centov;
+                }
+            }
+        }
+    }
+    
+    public void vloz(int eur, int centov) {
+        if (centov < 100) {
+            if (eur >= 0 && centov >= 0) {
+                this.sumaVCentoch += eur * 100 + centov;
+            }
+        }
+    }
+    
+    public String getStavUctu() {
+        return String.format("%d,%02d €", this.sumaVCentoch / 100, this.sumaVCentoch % 100);
+    }
 }
