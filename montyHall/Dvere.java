e0be141a20edaf359fbc963b614304b8a60d7c38
Jan Janech <jan.janech@kst.uniza.sk>
2020-11-18 12:45:00 +0100

    08a - spravny nahodny vyber vyhernych dveri

--- montyHall/Dvere.java
+++ montyHall/Dvere.java
@@ -1,13 +1,10 @@
-import java.util.Random;
-
 public class Dvere {
     private boolean vyhra;
     private boolean otvorene;
     private boolean vybrateHracom;
     
-    public Dvere() {
-        Random nahodneCisla = new Random();
-        this.vyhra = nahodneCisla.nextInt(3) == 0;
+    public Dvere(boolean vyhra) {
+        this.vyhra = vyhra;
         this.otvorene = false;
         this.vybrateHracom = false;
     }
--- montyHall/Sutaz.java
+++ montyHall/Sutaz.java
@@ -1,10 +1,14 @@
+import java.util.Random;
+
 public class Sutaz {
     private Dvere[] dvere;
     
     public Sutaz() {
+        Random nahodneCisla = new Random();
         this.dvere = new Dvere[3];
+        int vyherneDvere = nahodneCisla.nextInt(3);
         for (int i = 0; i < 3; i++) {
-            this.dvere[i] = new Dvere();
+            this.dvere[i] = new Dvere(i == vyherneDvere);
         }
     }
 }
