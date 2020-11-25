52f6764e63c84737d69b718c6b19e9b0eaeb9839
Jan Janech <jan.janech@kst.uniza.sk>
2020-11-25 13:47:10 +0100

    09b - pohyb kamena

--- nim/Kamen.java
+++ nim/Kamen.java
@@ -1,11 +1,17 @@
 public class Kamen {
     private Kruh kruh;
     
+    private int poziciaRiadok;
+    private int poziciaStlpec;
+    
     public Kamen(Sachovnica sachovnica) {
         this.kruh = new Kruh();
         this.kruh.zmenPriemer(18);
         this.kruh.posunVodorovne(-19 + (sachovnica.getSirka() - 1) * 20);
         this.kruh.posunZvisle(-59);
+        
+        this.poziciaRiadok = sachovnica.getVyska();
+        this.poziciaStlpec = sachovnica.getSirka();
     }
     
     public void zobraz() {
@@ -14,5 +20,15 @@ public class Kamen {
     
     public void skry() {
         this.kruh.skry();
+    }
+    
+    public void posunSa(int riadok, int stlpec) {
+        this.kruh.skry();
+        this.kruh.posunZvisle((this.poziciaRiadok - riadok) * 20);
+        this.kruh.posunVodorovne(-(this.poziciaStlpec - stlpec) * 20);
+        this.kruh.zobraz();
+        
+        this.poziciaRiadok = riadok;
+        this.poziciaStlpec = stlpec;
     }
 }
