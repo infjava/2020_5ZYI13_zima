c3f3f117599f7af55757ba691e1324b8e68d1adb
Jan Janech <jan.janech@kst.uniza.sk>
2020-12-08 14:46:14 +0100

    11a - zobrazovanie/skryvanie pixelov podla suradnice

--- tetris/Displej.java
+++ tetris/Displej.java
@@ -4,20 +4,30 @@ public class Displej {
     private static final int SIRKA = 10;
     private static final int VYSKA = 20;
     
+    private Stvorec[][] pixely;
+    
     public Displej() {
+        this.pixely = new Stvorec[Displej.VYSKA][Displej.SIRKA];
+        
         for (int y = 0; y < Displej.VYSKA; y++) {
             for (int x = 0; x < Displej.SIRKA; x++) {
                 Stvorec pixel = new Stvorec();
+                
                 pixel.zmenStranu(Displej.VELKOST_PIXELU - 1);
                 pixel.posunVodorovne(x * Displej.VELKOST_PIXELU - 60);
                 pixel.posunZvisle(y * Displej.VELKOST_PIXELU - 50);
                 pixel.zmenFarbu("black");
-                pixel.zobraz();
+                
+                this.pixely[y][x] = pixel;
             }
         }
     }
     
     public void nastav(int x, int y, boolean svieti) {
-        
+        if (svieti) {
+            this.pixely[y][x].zobraz();
+        } else {
+            this.pixely[y][x].skry();
+        }
     }
 }
