29b44b1be2c2eea1812c10d011eb00712d202904
Jan Janech <jan.janech@kst.uniza.sk>
2020-12-09 13:17:09 +0100

    11b - tetromino ma polohu x,y

--- tetris/Tetromino.java
+++ tetris/Tetromino.java
@@ -1,15 +1,21 @@
 public class Tetromino {
+    private int poziciaX;
+    private int poziciaY;
+    
     private boolean[][] polickoSvieti;
     
     public Tetromino(boolean[][] polickoSvieti) {
         this.polickoSvieti = polickoSvieti;
+        
+        this.poziciaX = (Displej.SIRKA - this.polickoSvieti[0].length) / 2;
+        this.poziciaY = 0;
     }
     
     public void nakresli() {
         for (int y = 0; y < this.polickoSvieti.length; y++) {
             for (int x = 0; x < this.polickoSvieti[y].length; x++) {
                 Displej displej = Displej.getDisplej();
-                displej.nastav(x, y, this.polickoSvieti[y][x]);
+                displej.nastav(x + this.poziciaX, y + this.poziciaY, this.polickoSvieti[y][x]);
             }
         }
     }
