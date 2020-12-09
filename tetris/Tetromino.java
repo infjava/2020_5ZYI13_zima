6fa590cf0bbe801c442dd896dcc107a83cc53927
Jan Janech <jan.janech@kst.uniza.sk>
2020-12-09 13:35:51 +0100

    11b - posuvanie tetromina po x osi

--- tetris/Tetromino.java
+++ tetris/Tetromino.java
@@ -11,6 +11,27 @@ public class Tetromino {
         this.poziciaY = 0;
     }
     
+    public void posunVlavo() {
+        this.zmaz();
+        this.poziciaX--;
+        this.nakresli();
+    }
+    
+    public void posunVpravo() {
+        this.zmaz();
+        this.poziciaX++;
+        this.nakresli();
+    }
+    
+    public void zmaz() {
+        for (int y = 0; y < this.polickoSvieti.length; y++) {
+            for (int x = 0; x < this.polickoSvieti[y].length; x++) {
+                Displej displej = Displej.getDisplej();
+                displej.nastav(x + this.poziciaX, y + this.poziciaY, false);
+            }
+        }
+    }
+    
     public void nakresli() {
         for (int y = 0; y < this.polickoSvieti.length; y++) {
             for (int x = 0; x < this.polickoSvieti[y].length; x++) {
